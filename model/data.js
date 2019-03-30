23c6f9b317287db29ad90af98946307f388020be
Erivaldo Oliveira Sobral - ESB <erivaldo.sobral@iteris.local>
2019-03-30 07:57:19 -0300

    First Commit

---
+++ model/data.js
@@ -0,0 +1,18 @@
+var data = [
+    {
+        date: "2019-03-08", 
+        badge: true, 
+        title: "Title", 
+        body: "Body", 
+        footer: "Footer", 
+        classname: "ClasName"
+    },
+    {
+        date: "2019-03-11", 
+        badge: true, 
+        title: "Title2", 
+        body: "Body2", 
+        footer: "Footer2", 
+        classname: "ClasName"
+    }
+]
